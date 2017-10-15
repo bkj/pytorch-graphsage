aaedf95c955d4287643e812bea87a33f500ec91e
bjohnson <bkj.322@gmail.com>
2017-10-14 21:46:20 -0400

    cleaning; refactoring

--- graphsage/supervised_models.py
+++ graphsage/supervised_models.py
@@ -41,8 +41,7 @@ class Model(object):
         
         self.name = name
         
-        logging = kwargs.get('logging', False)
-        self.logging = logging
+        self.logging = kwargs.get('logging', False)
         
         self.vars = {}
         self.placeholders = {}
@@ -79,9 +78,7 @@ class SupervisedGraphsage(Model):
         
         super(SupervisedGraphsage, self).__init__(**kwargs)
         
-        self.aggregator_cls = aggs[aggregator_type]
-        
-        # get info from placeholders...
+        self.aggregator = aggs[aggregator_type]
         self.inputs1    = placeholders["batch"]
         self.model_size = model_size
         self.adj_info   = adj
@@ -102,56 +99,55 @@ class SupervisedGraphsage(Model):
         
         self.degrees      = degrees
         self.concat       = concat
-        self.num_classes  = num_classes
-        self.sigmoid_loss = sigmoid_loss
-        
-        self.dims = [(0 if features is None else features.shape[1]) + identity_dim]
-        self.dims.extend([layer_infos[i].output_dim for i in range(len(layer_infos))])
-        
         self.batch_size   = placeholders["batch_size"]
         self.placeholders = placeholders
         self.layer_infos  = layer_infos
         
+        dims = [(0 if features is None else features.shape[1]) + identity_dim]
+        dims.extend([layer_infos[i].output_dim for i in range(len(layer_infos))])
+        
+        # Optimizer
         self.optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate)
         
-        self.build()
-        
-    def build(self):
+        # Sample
         samples1, support_sizes1 = self.sample(self.inputs1, self.layer_infos)
-        num_samples = [layer_info.num_samples for layer_info in self.layer_infos]
-        self.outputs1, self.aggregators = self.aggregate(samples1, [self.features], self.dims, num_samples,
-                support_sizes1, concat=self.concat, model_size=self.model_size)
         
-        dim_mult = 2 if self.concat else 1
+        # Aggregate
+        self.outputs1, self.aggregators = self.aggregate(
+            samples=samples1, 
+            input_features=[self.features],
+            dims=dims,
+            num_samples=[layer_info.num_samples for layer_info in self.layer_infos],
+            support_sizes=support_sizes1,
+            concat=self.concat,
+            model_size=self.model_size
+        )
         
+        # Normalize
         self.outputs1 = tf.nn.l2_normalize(self.outputs1, 1)
         
+        # Predict
         dim_mult = 2 if self.concat else 1
-        self.node_pred = Dense(dim_mult*self.dims[-1], self.num_classes, 
-                dropout=self.placeholders['dropout'],
-                act=lambda x : x)
-        
-        # TF graph management
+        self.node_pred = Dense(
+            dim_mult * dims[-1],
+            num_classes,
+            dropout=self.placeholders['dropout'],
+            act=lambda x : x
+        )
         self.node_preds = self.node_pred(self.outputs1)
         
-        self._loss()
-        grads_and_vars = self.optimizer.compute_gradients(self.loss)
-        clipped_grads_and_vars = [(tf.clip_by_value(grad, -5.0, 5.0) if grad is not None else None, var) 
-                for grad, var in grads_and_vars]
-        self.grad, _ = clipped_grads_and_vars[0]
-        self.opt_op = self.optimizer.apply_gradients(clipped_grads_and_vars)
-        self.preds = self.predict()
+        # --
+        # Define loss
         
-    def _loss(self):
-        # Weight decay loss
         for aggregator in self.aggregators:
             for var in aggregator.vars.values():
                 self.loss += FLAGS.weight_decay * tf.nn.l2_loss(var)
+        
         for var in self.node_pred.vars.values():
             self.loss += FLAGS.weight_decay * tf.nn.l2_loss(var)
        
         # classification loss
-        if self.sigmoid_loss:
+        if sigmoid_loss:
             self.loss += tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
                     logits=self.node_preds,
                     labels=self.placeholders['labels']))
@@ -162,41 +158,42 @@ class SupervisedGraphsage(Model):
             
         tf.summary.scalar('loss', self.loss)
         
-    def predict(self):
-        if self.sigmoid_loss:
-            return tf.nn.sigmoid(self.node_preds)
+        # --
+        # Gradients
+        
+        grads_and_vars = self.optimizer.compute_gradients(self.loss)
+        clipped_grads_and_vars = [(tf.clip_by_value(grad, -5.0, 5.0) if grad is not None else None, var) for grad, var in grads_and_vars]
+        self.grad, _ = clipped_grads_and_vars[0]
+        self.opt_op = self.optimizer.apply_gradients(clipped_grads_and_vars)
+        
+        # --
+        # Predictions
+        
+        if sigmoid_loss:
+            self.preds = tf.nn.sigmoid(self.node_preds)
         else:
-            return tf.nn.softmax(self.node_preds)
+            self.preds = tf.nn.softmax(self.node_preds)
             
         
-    def sample(self, inputs, layer_infos, batch_size=None):
-        """ Sample neighbors to be the supportive fields for multi-layer convolutions.
-
-        Args:
-            inputs: batch inputs
-            batch_size: the number of inputs (different for batch inputs and negative samples).
-        """
-        
-        if batch_size is None:
-            batch_size = self.batch_size
-        
+    def sample(self, inputs, layer_infos):
         samples = [inputs]
-        # size of convolution support at each layer per node
         support_size = 1
         support_sizes = [support_size]
         for k in range(len(layer_infos)):
             t = len(layer_infos) - k - 1
             support_size *= layer_infos[t].num_samples
             node = layer_infos[t].neigh_sampler((samples[k], layer_infos[t].num_samples))
-            samples.append(tf.reshape(node, [support_size * batch_size,]))
+            samples.append(tf.reshape(node, [support_size * self.batch_size,]))
             support_sizes.append(support_size)
         
         return samples, support_sizes
     
     def aggregate(self, samples, input_features, dims, num_samples, support_sizes, batch_size=None,
-            aggregators=None, name=None, concat=False, model_size="small"):
-        """ At each layer, aggregate hidden representations of neighbors to compute the hidden representations 
-            at next layer.
+            name=None, concat=False, model_size="small"):
+        """ 
+        
+        At each layer, aggregate hidden representations of neighbors to compute the hidden representations  at next layer.
+        
         Args:
             samples: a list of samples of variable hops away for convolving at each layer of the
                 network. Length is the number of layers + 1. Each is a vector of node indices.
@@ -206,37 +203,32 @@ class SupervisedGraphsage(Model):
             num_samples: list of number of samples for each layer.
             support_sizes: the number of nodes to gather information from for each layer.
             batch_size: the number of inputs (different for batch inputs and negative samples).
+        
         Returns:
             The hidden representation at the final layer for all nodes in batch
         """
         
-        if batch_size is None:
-            batch_size = self.batch_size
+        batch_size = batch_size if batch_size else self.batch_size
             
         # length: number of layers + 1
         hidden = [tf.nn.embedding_lookup(input_features, node_samples) for node_samples in samples]
         
-        new_agg = aggregators is None
-        if new_agg:
-            aggregators = []
-        
+        aggregators = []
         for layer in range(len(num_samples)):
-            if new_agg:
-                dim_mult = 2 if concat and (layer != 0) else 1
-                # aggregator at current layer
-                if layer == len(num_samples) - 1:
-                    aggregator = self.aggregator_cls(dim_mult*dims[layer], dims[layer+1], act=lambda x : x,
-                            dropout=self.placeholders['dropout'], 
-                            name=name, concat=concat, model_size=model_size)
-                else:
-                    aggregator = self.aggregator_cls(dim_mult*dims[layer], dims[layer+1],
-                            dropout=self.placeholders['dropout'], 
-                            name=name, concat=concat, model_size=model_size)
-                aggregators.append(aggregator)
+            dim_mult = 2 if concat and (layer != 0) else 1
+            # aggregator at current layer
+            if layer == len(num_samples) - 1:
+                aggregator = self.aggregator(dim_mult*dims[layer], dims[layer+1], act=lambda x : x,
+                        dropout=self.placeholders['dropout'], 
+                        name=name, concat=concat, model_size=model_size)
             else:
-                aggregator = aggregators[layer]
+                aggregator = self.aggregator(dim_mult*dims[layer], dims[layer+1],
+                        dropout=self.placeholders['dropout'], 
+                        name=name, concat=concat, model_size=model_size)
+            aggregators.append(aggregator)
             
             # hidden representation at current layer for all support nodes that are various hops away
+            
             next_hidden = []
             # as layer increases, the number of support nodes needed decreases
             for hop in range(len(num_samples) - layer):
--- graphsage/supervised_train.py
+++ graphsage/supervised_train.py
@@ -16,8 +16,21 @@ from sklearn import metrics
 
 from graphsage.utils import load_data
 from graphsage.minibatch import NodeMinibatchIterator
-from graphsage.neigh_samplers import UniformNeighborSampler
 from graphsage.supervised_models import SupervisedGraphsage, SAGEInfo
+
+# --
+# Helpers
+
+class UniformNeighborSampler(object):
+    def __init__(self, adj_info, **kwargs):
+        self.adj_info = adj_info
+        
+    def __call__(self, inputs):
+        ids, num_samples = inputs
+        adj_lists = tf.nn.embedding_lookup(self.adj_info, ids)
+        adj_lists = tf.transpose(tf.random_shuffle(tf.transpose(adj_lists)))
+        return tf.slice(adj_lists, [0,0], [-1, num_samples])
+
 
 # --
 # Params
@@ -51,7 +64,6 @@ flags.DEFINE_integer('samples_2', 10, 'number of samples in layer 2')
 flags.DEFINE_integer('samples_3', 0, 'number of users samples in layer 3. (Only for mean model)')
 flags.DEFINE_integer('dim_1', 128, 'Size of output dim (final is 2x this, if using concat)')
 flags.DEFINE_integer('dim_2', 128, 'Size of output dim (final is 2x this, if using concat)')
-flags.DEFINE_boolean('random_context', True, 'Whether to use random context or direct edges')
 flags.DEFINE_integer('batch_size', 512, 'minibatch size.')
 flags.DEFINE_boolean('sigmoid', False, 'whether to use sigmoid loss')
 flags.DEFINE_integer('identity_dim', 0, 'Set to positive value to use identity embedding features of that dimension. Default 0.')
@@ -62,7 +74,6 @@ flags.DEFINE_integer('validate_iter', 5000, "how often to run a validation minib
 flags.DEFINE_integer('validate_batch_size', 256, "how many nodes per validation sample.")
 flags.DEFINE_integer('gpu', 1, "which gpu to use.")
 flags.DEFINE_integer('print_every', 5, "How often to print training info.")
-flags.DEFINE_integer('max_total_steps', 10**10, "Maximum total number of iterations")
 
 all_models = [
     'graphsage_mean',
@@ -86,14 +97,6 @@ def calc_f1(y_true, y_pred):
     
     return metrics.f1_score(y_true, y_pred, average="micro"), metrics.f1_score(y_true, y_pred, average="macro")
 
-# Define model evaluation function
-def evaluate(sess, model, minibatch_iter, size=None):
-    t_test = time.time()
-    feed_dict_val, labels = minibatch_iter.node_val_feed_dict(size)
-    node_outs_val = sess.run([model.preds, model.loss], feed_dict=feed_dict_val)
-    mic, mac = calc_f1(labels, node_outs_val[0])
-    return node_outs_val[1], mic, mac, (time.time() - t_test)
-
 def log_dir():
     log_dir = FLAGS.base_log_dir + "/sup-" + FLAGS.train_prefix.split("/")[-2]
     log_dir += "/{model:s}_{model_size:s}_{lr:0.4f}/".format(
@@ -101,48 +104,22 @@ def log_dir():
         model_size=FLAGS.model_size,
         lr=FLAGS.learning_rate
     )
+    
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
     
     return log_dir
 
-def incremental_evaluate(sess, model, minibatch_iter, size, test=False):
-    t_test = time.time()
-    finished = False
-    val_losses = []
-    val_preds = []
-    labels = []
-    iter_num = 0
-    finished = False
-    while not finished:
-        feed_dict_val, batch_labels, finished, _  = minibatch_iter.incremental_node_val_feed_dict(size, iter_num, test=test)
-        node_outs_val = sess.run([model.preds, model.loss], feed_dict=feed_dict_val)
-        val_preds.append(node_outs_val[0])
-        labels.append(batch_labels)
-        val_losses.append(node_outs_val[1])
-        iter_num += 1
-    
-    val_preds = np.vstack(val_preds)
-    labels = np.vstack(labels)
-    f1_scores = calc_f1(labels, val_preds)
-    return np.mean(val_losses), f1_scores[0], f1_scores[1], (time.time() - t_test)
-
-
-def construct_placeholders(num_classes):
-    return {
-        'labels'     : tf.placeholder(tf.float32, shape=(None, num_classes), name='labels'),
-        'batch'      : tf.placeholder(tf.int32, shape=(None), name='batch1'),
-        'dropout'    : tf.placeholder_with_default(0., shape=(), name='dropout'),
-        'batch_size' : tf.placeholder(tf.int32, name='batch_size'),
-    }
-
 
 def train(train_data, test_data=None):
     
-    G          = train_data[0]
-    features   = train_data[1]
-    id_map     = train_data[2]
-    class_map  = train_data[4]
+    total_steps   = 0
+    
+    G             = train_data[0]
+    features      = train_data[1]
+    id_map        = train_data[2]
+    context_pairs = train_data[3]
+    class_map     = train_data[4]
     
     if isinstance(list(class_map.values())[0], list):
         num_classes = len(list(class_map.values())[0])
@@ -153,8 +130,12 @@ def train(train_data, test_data=None):
     if features is not None:
         features = np.vstack([features, np.zeros((features.shape[1],))])
     
-    context_pairs = train_data[3] if FLAGS.random_context else None
-    placeholders = construct_placeholders(num_classes)
+    placeholders = {
+        'labels'     : tf.placeholder(tf.float32, shape=(None, num_classes), name='labels'),
+        'batch'      : tf.placeholder(tf.int32, shape=(None), name='batch1'),
+        'dropout'    : tf.placeholder_with_default(0., shape=(), name='dropout'),
+        'batch_size' : tf.placeholder(tf.int32, name='batch_size'),
+    }
     
     minibatch = NodeMinibatchIterator(
         G,
@@ -276,103 +257,77 @@ def train(train_data, test_data=None):
             aggregator_type="meanpool",
         )
     
+    # Initialize session
     config = tf.ConfigProto(log_device_placement=FLAGS.log_device_placement)
     config.gpu_options.allow_growth = True
-    #config.gpu_options.per_process_gpu_memory_fraction = GPU_MEM_FRACTION
     config.allow_soft_placement = True
-    
-    # Initialize session
     sess = tf.Session(config=config)
     merged = tf.summary.merge_all()
-    summary_writer = tf.summary.FileWriter(log_dir(), sess.graph)
      
     # Init variables
     sess.run(tf.global_variables_initializer())
     
     # Train model
-    total_steps = 0
     avg_time = 0.0
-    epoch_val_costs = []
     
     train_adj_info = tf.assign(adj_info, minibatch.adj)
-    val_adj_info = tf.assign(adj_info, minibatch.test_adj)
+    val_adj_info = tf.assign(adj_info, minibatch.val_adj)
     for epoch in range(FLAGS.epochs): 
-        minibatch.shuffle() 
+        minibatch.shuffle()
         
         iter = 0
         print('Epoch: %04d' % (epoch + 1))
-        epoch_val_costs.append(0)
-        while not minibatch.end():
+        while not minibatch.is_done:
             # Construct feed dictionary
-            feed_dict, labels = minibatch.next_minibatch_feed_dict()
-            feed_dict.update({placeholders['dropout']: FLAGS.dropout})
+            feed_dict, labels = minibatch.get_train_batch()
+            feed_dict.update({
+                placeholders['dropout']: FLAGS.dropout
+            })
             
             t = time.time()
             
             # Training step
             outs = sess.run([merged, model.opt_op, model.loss, model.preds], feed_dict=feed_dict)
-            train_cost = outs[2]
             
             if iter % FLAGS.validate_iter == 0:
-                # Validation
                 sess.run(val_adj_info.op)
-                if FLAGS.validate_batch_size == -1:
-                    val_cost, val_f1_mic, val_f1_mac, duration = incremental_evaluate(sess, model, minibatch, FLAGS.batch_size)
-                else:
-                    val_cost, val_f1_mic, val_f1_mac, duration = evaluate(sess, model, minibatch, FLAGS.validate_batch_size)
+                
+                val_feed_dict, val_labels = minibatch.get_eval_batch(size=FLAGS.validate_batch_size, test=False)
+                val_node_outs = sess.run([model.preds, model.loss], feed_dict=val_feed_dict)
+                val_f1_mic, val_f1_mac = calc_f1(val_labels, val_node_outs[0])
                 
                 sess.run(train_adj_info.op)
-                epoch_val_costs[-1] += val_cost
-                
-            if total_steps % FLAGS.print_every == 0:
-                summary_writer.add_summary(outs[0], total_steps)
-            
-            # Print results
-            avg_time = (avg_time * total_steps + time.time() - t) / (total_steps + 1)
             
             if total_steps % FLAGS.print_every == 0:
                 train_f1_mic, train_f1_mac = calc_f1(labels, outs[-1])
-                print("Iter:", '%04d' % iter, 
-                      "train_loss=", "{:.5f}".format(train_cost),
-                      "train_f1_mic=", "{:.5f}".format(train_f1_mic), 
-                      "train_f1_mac=", "{:.5f}".format(train_f1_mac), 
-                      "val_loss=", "{:.5f}".format(val_cost),
-                      "val_f1_mic=", "{:.5f}".format(val_f1_mic), 
-                      "val_f1_mac=", "{:.5f}".format(val_f1_mac), 
-                      "time=", "{:.5f}".format(avg_time))
+                print(
+                    "Iter:", '%04d' % iter, 
+                    "train_f1_mic=", "{:.5f}".format(train_f1_mic),
+                    "train_f1_mac=", "{:.5f}".format(train_f1_mac),
+                    "val_f1_mic=", "{:.5f}".format(val_f1_mic),
+                    "val_f1_mac=", "{:.5f}".format(val_f1_mac),
+                )
             
             iter += 1
             total_steps += 1
-            
-            if total_steps > FLAGS.max_total_steps:
-                break
-                
-        if total_steps > FLAGS.max_total_steps:
-                break
     
     sess.run(val_adj_info.op)
     
-    val_cost, val_f1_mic, val_f1_mac, duration = incremental_evaluate(sess, model, minibatch, FLAGS.batch_size, test=False)
-    print(
-        "Full validation stats:", 
-        "loss=", "{:.5f}".format(val_cost),
-        "f1_micro=", "{:.5f}".format(val_f1_mic),
-        "f1_macro=", "{:.5f}".format(val_f1_mac),
-        "time=", "{:.5f}".format(duration),
-    )
-    open(log_dir() + "val_stats.txt", "w").write("loss={:.5f} f1_micro={:.5f} f1_macro={:.5f} time={:.5f}".format(val_cost, val_f1_mic, val_f1_mac, duration))
+    # Final validation performance
+    val_feed_dict, val_labels = minibatch.get_eval_batch(size=None, test=False)
+    val_node_outs = sess.run([model.preds, model.loss], feed_dict=val_feed_dict)
+    val_f1_mic, val_f1_mac = calc_f1(val_labels, val_node_outs[0])
+    print("\nval_f1_mic={:.5f} val_f1_mac={:.5f}".format(val_f1_mic, val_f1_mac))
     
-    val_cost, val_f1_mic, val_f1_mac, duration = incremental_evaluate(sess, model, minibatch, FLAGS.batch_size, test=True)
-    open(log_dir() + "test_stats.txt", "w").write("loss={:.5f} f1_micro={:.5f} f1_macro={:.5f}".format(val_cost, val_f1_mic, val_f1_mac))
-
-
-def main(argv=None):
-    assert FLAGS.model in all_models, 'Error: model name unrecognized.'
-    print("Loading training data..")
-    train_data = load_data(FLAGS.train_prefix)
-    print("Done loading training data..")
-    train(train_data)
+    # Final test performance
+    test_feed_dict, test_labels = minibatch.get_eval_batch(size=None, test=True)
+    test_node_outs = sess.run([model.preds, model.loss], feed_dict=test_feed_dict)
+    test_f1_mic, test_f1_mac = calc_f1(test_labels, test_node_outs[0])
+    print("\ntest_f1_mic={:.5f} test_f1_mac={:.5f}".format(test_f1_mic, test_f1_mac))
 
 
 if __name__ == '__main__':
-    tf.app.run()
+    assert FLAGS.model in all_models, 'Error: model name unrecognized.'
+    print("Loading training data..")
+    train_data = load_data(FLAGS.train_prefix)
+    train(train_data)
