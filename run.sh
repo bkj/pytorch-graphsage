6a0570621af4598bb6e01775d98ff401016279b8
bjohnson <bkj.322@gmail.com>
2017-10-17 12:51:22 -0400

    ...

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@
 
 python -m train \
     --data-path ./data/reddit/ \
-    --aggregator-class mean \
-    --lr-init 0.005
-
+    --aggregator-class attention \
+    --lr-init 0.005 \
+    --lr-schedule linear
 
